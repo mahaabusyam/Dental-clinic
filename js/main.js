42cae3b39b301039c92a2e5e622810135d518879
Maha ahmed abu syam <maha.abu.syam@gmail.com>
2026-04-15 15:32:34 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -9,3 +9,26 @@ document.querySelectorAll(".nav-links a").forEach(link => {
         nav.classList.remove("active");
     });
 });
+const elements = document.querySelectorAll(
+    '.hidden, .fade-left, .fade-right, .zoom-in, .solution-card, .price-card, .learn-card'
+);
+
+const observer = new IntersectionObserver((entries, obs) => {
+    entries.forEach((entry) => {
+
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show');
+            if (
+                entry.target.classList.contains('solution-card') ||
+                entry.target.classList.contains('price-card') ||
+                entry.target.classList.contains('learn-card')
+            ) {
+                obs.unobserve(entry.target);
+            }
+        }
+    });
+}, {
+    threshold: 0.2
+});
+
+elements.forEach(el => observer.observe(el));
